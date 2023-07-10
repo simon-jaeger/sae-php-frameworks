9fb1387cd246ac38c143435ba535c66b54c5b639
Simon Jäger <simon-jaeger@users.noreply.github.com>
2023-07-10 20:17:09 +0200

    j

--- wdd0322/database/seeders/DatabaseSeeder.php
+++ wdd0322/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
 use Illuminate\Database\Seeder;
 
 // faker: https://fakerphp.github.io/formatters/text-and-paragraphs/
 
 class DatabaseSeeder extends Seeder {
   function run() {
+    for ($i = 0; $i < 10; $i++) {
+      Note::create([
+        'title' => fake()->word(),
+        'content' => fake()->sentences(4, true),
+      ]);
+    }
   }
 }
