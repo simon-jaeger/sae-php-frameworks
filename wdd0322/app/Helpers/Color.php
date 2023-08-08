7c31e85d5bf17ea15414552cb35abc19399c517a
Simon Jäger <simon-jaeger@users.noreply.github.com>
2023-08-08 20:01:46 +0200

    j

--- wdd0322/app/Helpers/Color.php
+++ wdd0322/app/Helpers/Color.php
@@ -13,11 +13,19 @@ class Color {
     return '#' . Str::padLeft(dechex(mt_rand(0, 16777215)), 6, '0');
   }
 
+  // invert('#00bbff')
   static function invert(string $color) {
-    // TODO: return inverted color (#000000 --> #ffffff)
+    $red = hexdec(substr($color, 1, 2)); // '00' --> 0
+    $green = hexdec(substr($color, 3, 2)); // 'bb' --> 187
+    $blue = hexdec(substr($color, 5, 2));// 'ff' --> 255
+    $t = fn($x) => Str::padLeft(dechex(255 - $x), 2, '0');
+    return '#' . $t($red) . $t($green) . $t($blue);
   }
 
   static function isGray(string $color) {
-    // TODO: return if color is a perfect gray (red, green, blue identical)
+    $red = substr($color, 1, 2);
+    $green = substr($color, 3, 2);
+    $blue = substr($color, 5, 2);
+    return $red === $blue && $blue === $green;
   }
 }
